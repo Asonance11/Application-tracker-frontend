85f0c120a792846488510f4ea2728dd8f91feeeb
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-20 12:18:10 +0100

    Update styles

--- src/components/common/Loaders.tsx
+++ src/components/common/Loaders.tsx
@@ -1,5 +1,5 @@
 import { Loader2 } from "lucide-react";
 
 export const Loading = () => {
-  return <Loader2 className="h-4 w-4 animate-spin text-main" />;
+  return <Loader2 className="h-4 w-4 animate-spin" />;
 };
