ddd08120ec6c4909f563fb0c4d0ec1cc4ca6a86d
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-20 13:23:01 +0100

    Edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-
 export default function Home() {
+  // Redirect to dashboar i logged in or redirect to login page if not
   return (
     <main>
       <h1>This is home</h1>
