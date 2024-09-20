17dc07b47ae0b423249024422df111bb6ff73c9d
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-20 11:40:52 +0100

    More setup

---
+++ src/app/(main)/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+
+const DashBoardPage = () => {
+  return (
+    <main>
+      <h1>This is the where all the fun happens</h1>
+    </main>
+  );
+};
+
+export default DashBoardPage;
