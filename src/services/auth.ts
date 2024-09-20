286d3c14ad4277da241723ea18a5a1198bc7008d
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-20 13:22:13 +0100

    Api call

---
+++ src/services/auth.ts
@@ -0,0 +1,36 @@
+import { API_URL } from "@/lib/config";
+import { User, UserInput } from "@/types/index";
+import axios from "axios";
+import { toast } from "sonner";
+
+export async function Login(values: UserInput): Promise<User | null> {
+  try {
+    const response = await axios.post(
+      `${API_URL}/login`,
+      {
+        username: values.username,
+        password: values.password,
+      },
+      {
+        withCredentials: true, // This is important for handling cookies
+      },
+    );
+
+    if (response.data.user) {
+      toast.success("Logged in successfully");
+      return response.data.user;
+    } else {
+      throw new Error("Unexpected response from server");
+    }
+  } catch (error) {
+    console.error("[LOGIN_ERROR]", error);
+
+    if (axios.isAxiosError(error) && error.response?.data?.error) {
+      toast.error(error.response.data.error);
+    } else {
+      toast.error("Something went wrong, please try again");
+    }
+
+    return null;
+  }
+}
