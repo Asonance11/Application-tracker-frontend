268ebfc65e154d4e268d8fe3c0b05684ef36322a
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-20 13:22:02 +0100

    Add types

---
+++ src/types/index.ts
@@ -0,0 +1,40 @@
+export interface BaseModel {
+  id: number;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string | null;
+}
+
+export interface User extends BaseModel {
+  username: string;
+  jobs: Job[];
+}
+
+export interface Job extends BaseModel {
+  role: string;
+  companyName: string;
+  expectedSalary: number;
+  status: JobStatus;
+  userId: number;
+}
+
+export enum JobStatus {
+  Applied = "applied",
+  GotResponse = "got_response",
+  GotInterview = "got_interview",
+  FailedInterview = "failed_interview",
+  Rejected = "rejected",
+  GotOffer = "got_offer",
+}
+
+export interface UserInput {
+  username: string;
+  password: string;
+}
+
+export interface JobInput {
+  role: string;
+  companyName: string;
+  expectedSalary: number;
+  status: JobStatus;
+}
