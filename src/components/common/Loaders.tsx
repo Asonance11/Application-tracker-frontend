bd50ace7eb8c1ddcc95e44d9825396e876549d54
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-22 15:27:09 +0100

    Add full screen loader

--- src/components/common/Loaders.tsx
+++ src/components/common/Loaders.tsx
@@ -3,3 +3,11 @@ import { Loader2 } from "lucide-react";
 export const Loading = () => {
   return <Loader2 className="h-4 w-4 animate-spin" />;
 };
+
+export const FullPageLoader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-bg">
+      <Loader2 className="h-12 w-12 animate-spin text-main" />
+    </div>
+  );
+};
