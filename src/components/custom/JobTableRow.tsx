7f6bf02dbcc33071de757c726d0c493c38353b65
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-24 13:04:20 +0100

    Add actions dropdown

--- src/components/custom/JobTableRow.tsx
+++ src/components/custom/JobTableRow.tsx
@@ -1,5 +1,14 @@
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Job, JobFromDB } from "@/types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Ellipsis } from "lucide-react";
 
 interface JobTableRowProps {
   job: JobFromDB;
@@ -12,6 +21,19 @@ export const JobTableRow: React.FC<JobTableRowProps> = ({ job }) => {
       <TableCell>{job.CompanyName}</TableCell>
       <TableCell>{job.ExpectedSalary}</TableCell>
       <TableCell>{job.Status}</TableCell>
+      <TableCell>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Ellipsis />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </TableCell>
     </TableRow>
   );
 };
