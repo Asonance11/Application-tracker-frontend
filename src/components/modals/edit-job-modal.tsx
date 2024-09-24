606e1e81fe3a08273fd0440c61b41bcd95cc3201
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-24 13:46:20 +0100

    Edit job modal

--- src/components/modals/edit-job-modal.tsx
+++ src/components/modals/edit-job-modal.tsx
@@ -1,10 +1,178 @@
 "use client";
 
+import { useModal } from "@/store/use-modal-store";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import { JobStatus } from "@/types";
+
+const formSchema = z.object({
+  role: z.string().min(1, {
+    message: "Job role cannot be empty",
+  }),
+  companyName: z.string().min(1, {
+    message: "Company name cannot be empty",
+  }),
+  expectedSalary: z.number({
+    required_error: "Salary cannot be empty",
+  }),
+  status: z.string({
+    required_error: "Status cannot be empty",
+  }),
+});
+
 const EditJobModal = () => {
+  const { isOpen, type, data, onClose } = useModal();
+
+  const isModalOpen = isOpen && type === "editJobModal";
+
+  const { job } = data;
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      role: "",
+      companyName: "",
+      expectedSalary: 0,
+      status: "",
+    },
+  });
+
+  useEffect(() => {
+    if (job) {
+      form.setValue("role", job.Role);
+      form.setValue("companyName", job.CompanyName);
+      form.setValue("expectedSalary", job.ExpectedSalary);
+      form.setValue("status", job.Status);
+    }
+  }, [form, job]);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
+
   return (
-    <div>
-      <h1>Edit Job modal</h1>
-    </div>
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Job Application</DialogTitle>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="role"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Role</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="companyName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Company Name</FormLabel>
+                  <FormControl>
+                    <Input type="text" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="expectedSalary"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Expected Salary</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="number"
+                      {...field}
+                      onChange={(e) => {
+                        const value = e.target.value;
+                        field.onChange(value === "" ? null : Number(value));
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Job Status</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select job status" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      {Object.entries(JobStatus).map(([key, value]) => (
+                        <SelectItem key={value} value={value}>
+                          {key.replace(/([A-Z])/g, " $1").trim()}
+                        </SelectItem>
+                      ))}
+                    </SelectContent>
+                  </Select>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit" className="w-full">
+              Edit
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 };
 
