96d4ab873cbcfce769345ce444428581df958e4e
Oluwadabira <adigunsamuelaso@gmail.com>
2024-09-25 16:12:24 +0100

    Implement delete and format messages

--- src/components/custom/JobTableRow.tsx
+++ src/components/custom/JobTableRow.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { TableCell, TableRow } from "@/components/ui/table";
-import { Job, JobFromDB } from "@/types";
+import { JobFromDB, JobStatus } from "@/types";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,6 +11,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Ellipsis } from "lucide-react";
 import { useModal } from "@/store/use-modal-store";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteJob } from "@/services/jobs";
+import { toast } from "sonner";
+
+// Utility function to format the job status
+const formatJobStatus = (status: JobStatus): string => {
+  const statusMap: Record<JobStatus, string> = {
+    [JobStatus.Applied]: "Applied",
+    [JobStatus.GotResponse]: "Got Response",
+    [JobStatus.GotInterview]: "Got Interview",
+    [JobStatus.FailedInterview]: "Failed Interview",
+    [JobStatus.Rejected]: "Rejected",
+    [JobStatus.GotOffer]: "Got Offer",
+  };
+  return statusMap[status] || status;
+};
 
 interface JobTableRowProps {
   job: JobFromDB;
@@ -17,13 +34,34 @@ interface JobTableRowProps {
 
 export const JobTableRow: React.FC<JobTableRowProps> = ({ job }) => {
   const { onOpen } = useModal();
+  const queryClient = useQueryClient();
+
+  const deleteJobMutation = useMutation({
+    mutationFn: deleteJob,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["jobApplications"] });
+    },
+    onError: (error) => {
+      console.error("Failed to delete job application:", error);
+    },
+  });
+
+  const handleDelete = async () => {
+    toast.promise(deleteJobMutation.mutateAsync(job.ID), {
+      loading: "Deleting job...",
+      success: "Job deleted successfully",
+      error: "Failed to delete job application",
+    });
+  };
 
   return (
     <TableRow>
       <TableCell className="capitalize">{job.Role}</TableCell>
       <TableCell className="capitalize">{job.CompanyName}</TableCell>
       <TableCell className="capitalize">{job.ExpectedSalary}</TableCell>
-      <TableCell className="capitalize">{job.Status}</TableCell>
+      <TableCell className="capitalize">
+        {formatJobStatus(job.Status)}
+      </TableCell>
       <TableCell>
         <DropdownMenu>
           <DropdownMenuTrigger>
@@ -35,7 +73,7 @@ export const JobTableRow: React.FC<JobTableRowProps> = ({ job }) => {
             <DropdownMenuItem onClick={() => onOpen("editJobModal", { job })}>
               Edit
             </DropdownMenuItem>
-            <DropdownMenuItem>Delete</DropdownMenuItem>
+            <DropdownMenuItem onClick={handleDelete}>Delete</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </TableCell>
